4f0036d5609a44798635a0588eacaedd8268e060
Galkon <thegalkon@gmail.com>
2015-08-22 14:55:24 -0700

    -ReadWriteFile updated to use RandomAccessFile

--- api/src/com/nova/file/ReadWriteFile.java
+++ api/src/com/nova/file/ReadWriteFile.java
@@ -3,7 +3,10 @@ package com.nova.file;
 import com.nova.buffer.DataBuffer;
 import com.nova.buffer.WriteBuffer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 /**
  * "The real danger is not that computers will begin to think like men, but that men will begin to think like computers." – Sydney Harris
@@ -16,9 +19,15 @@ public abstract class ReadWriteFile {
     }
 
     private final String filePath;
+    private RandomAccessFile randomAccessFile;
 
     public ReadWriteFile(String filePath) {
         this.filePath = filePath;
+        try {
+            this.randomAccessFile = new RandomAccessFile(filePath, "rwd");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public abstract void writeToBuffer(WriteBuffer buffer);
@@ -32,6 +41,12 @@ public abstract class ReadWriteFile {
             e.printStackTrace();
             return false;
         }
+        try {
+            this.randomAccessFile.write(buffer.toByteArray());
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FileUtils.writeToFile(this.filePath, buffer.toByteArray());
         return true;
     }
@@ -42,6 +57,15 @@ public abstract class ReadWriteFile {
         if (!FileUtils.fileExists(this.filePath)) {
             System.err.print(this.filePath + " does not exist!");
             return false;
+        }
+        try {
+            byte[] data = new byte[(int) new File(this.filePath).length()];
+            this.randomAccessFile.readFully(data);
+            DataBuffer buffer = DataBuffer.wrap(data);
+            this.readFromBuffer(buffer);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         DataBuffer buffer = DataBuffer.wrap(FileUtils.fileToByteArray(this.filePath));
         this.readFromBuffer(buffer);
